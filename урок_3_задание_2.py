4743b18f56b90825001435af99e9469682555084
Alina43033 <aigul16kr@mail.ru>
2025-09-21 10:11:58 +0300

    урок_3_задание_2.py

--- урок_3_задание_2.py
+++ урок_3_задание_2.py
@@ -1,15 +1,9 @@
 from smartphone import Smartphone
-
-phone1 = Smartphone
-phone2 = Smartphone
-phone3 = Smartphone
-phone4 = Smartphone
-phone5 = Smartphone
-phone1 = ("Apple", "SE2020", "+79992355587")
-phone2 = ("Lenovo", "10", "+79125698453")
-phone3 = ("Honor", "Deluxe", "+79089089898")
-phone4 = ("Samsung", "Galaxy S4", "+79876543211")
-phone5 = ("Redmi", "Note", "+79505336569")
+phone1 =  Smartphone("Apple", "SE2020", "+79992355587")
+phone2 =  Smartphone("Lenovo", "10", "+79125698453")
+phone3 =  Smartphone("Honor", "Deluxe", "+79089089898")
+phone4 =  Smartphone("Samsung", "Galaxy S4", "+79876543211")
+phone5 =  Smartphone("Redmi", "Note", "+79505336569")
 
 catalog = [phone1, phone2, phone3, phone4, phone5]
 
